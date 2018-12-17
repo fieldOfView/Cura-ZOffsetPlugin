6c5f0fa8a447353606e5477f58a8f0874b205ea5
fieldOfView <aldo@fieldofview.com>
2018-12-17 14:32:43 +0100

    Reove unused import

--- __init__.py
+++ __init__.py
@@ -2,8 +2,7 @@
 # The ZOffsetPlugin is released under the terms of the AGPLv3 or higher.
 
 from . import ZOffsetPlugin
-from UM.i18n import i18nCatalog
-i18n_catalog = i18nCatalog("ZOffsetPlugin")
+
 
 def getMetaData():
     return {}
